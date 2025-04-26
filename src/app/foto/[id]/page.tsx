0e8b915cfe07b99d1e2cbcd0de5a2665df295c72
Gustavo Larsen <guslarsen@gmail.com>
2025-04-26 11:08:11 -0300

    refactor: add FotoIdPageProps Promise

--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,7 +1,12 @@
-export default function FotoIdPage() {
+type FotoIdPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function FotoIdPage({ params }: FotoIdPageProps) {
+  const { id } = await params;
   return (
     <main>
-      <h1>FotoIdPage</h1>
+      <h1>FotoIdPage {id}</h1>
     </main>
   );
 }
