e03e96d6757b9b6c741b31460974b1019b4439c3
Gustavo Larsen <guslarsen@gmail.com>
2025-04-26 17:38:13 -0300

    feat: add context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,31 @@ import { type_second } from '@/utils/fonts';
 import './globals.css';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import { UserContextProvider } from '@/context/userContext';
+import userGet from '@/actions/userGet';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
   description: 'Rede social para cães',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data } = await userGet();
+
   return (
     <html lang="pt-BR">
       <body className={type_second.variable}>
-        <div className="App">
-          <Header />
-          <main className="AppBody">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={data}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );
