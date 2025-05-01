8d4d1241a6f77e147a44ff4af4527e827baf8a7c
Gustavo Larsen <guslarsen@gmail.com>
2025-05-01 15:05:37 -0300

    feat: finalizacao do projeto

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,7 +1,35 @@
-export default async function EstatisiticasPage() {
+'use client';
+
+import React from 'react';
+import statsGet, { StatsData } from '@/actions/statsGet';
+import dynamic from 'next/dynamic';
+
+const ContaEstatisticas = dynamic(
+  () => import('@/components/conta/contaEstatisticas'),
+  {
+    loading: () => <p>Carregando...</p>,
+    ssr: false,
+  },
+);
+
+export default function EstatisticasPage() {
+  const [data, setData] = React.useState<StatsData[] | null>(null);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const result = await statsGet();
+      if (result.ok) {
+        setData(result.data);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (!data) return null;
+
   return (
-    <main>
-      <h1>Estatisitica</h1>
-    </main>
+    <section>
+      <ContaEstatisticas data={data} />
+    </section>
   );
 }
--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,7 +1,19 @@
-export default async function PerfilUserPage() {
+import photosGet from '@/actions/photosGet';
+import Feed from '@/components/feed';
+
+export default async function PerfilUserPage({
+  params,
+}: {
+  params: Promise<{ user: string }>;
+}) {
+  const { user } = await params;
+  const { data } = await photosGet({ user });
+
+  if (!data) return null;
   return (
-    <main>
-      <h1>PerfilUserPage</h1>
-    </main>
+    <section className="container mainSection">
+      <h1 className="title">{user}</h1>
+      <Feed photos={data} user={user} />
+    </section>
   );
 }
